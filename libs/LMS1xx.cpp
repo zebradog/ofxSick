281ab68b6a4a5ec7776c13094e6c2d017195e196
Matt Cook <matt@zebradog.com>
2013-08-28 16:18:25 -0500

    Removed unnecessary includes in LMS1xx

--- libs/LMS1xx.cpp
+++ libs/LMS1xx.cpp
@@ -22,10 +22,6 @@
  ***************************************************************************/
 
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <unistd.h>
